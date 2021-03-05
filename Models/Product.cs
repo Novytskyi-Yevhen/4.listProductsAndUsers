a32d9a753a839f3b1ddace6d79bd4a564e825af8
Jurii Pernarovskyi <j.pernarovskyi@gmail.com>
2021-03-05 19:34:05 +0200

    Task Validation. Applied description validation (if set, length sould be longer than 2 symbols) and product Type validation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +10,17 @@ namespace ProductsValidation.Models
 {
     public class Product
     {
-        public enum Category { Toy, Technique, Clothes, Transport}
+        public enum Category { Toy, Technique, Clothes, Transport }
 
         public int Id { get; set; }
         public Category Type { get; set; }
+        [Required]
         public string Name { get; set; }
+        [MinLength(2, ErrorMessage = "The value must contain more than 2 characters.")]
+        [Description]
         public string Description { get; set; }
+        [Range(0, 100000.00)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
     }
 }
